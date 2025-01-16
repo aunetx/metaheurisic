853fb72daec5ca6f2a34c0c45f157baa609431f9
Robin <171543700+RobinCurinier@users.noreply.github.com>
2025-01-16 22:14:55 +0100

    fix ?

--- genetic.py
+++ genetic.py
@@ -1,20 +1,25 @@
 # %%
+#imports
 
 import numpy as np
 import matplotlib.pyplot as plt
 import math
 import copy
-from tqdm import tqdm
+import igraph
+from time import time
 
 from utilitaires import *
+import scoreEtudiant
 
-# %%
+# %% definitions 
 
-
+# definitions
 def mutation_echange(parcours):
     """
-    Échange deux villes du parcours au hasard (sauf la première).
+    Échange deux villes du parcours au hasard (sauf la première) 
+    avec une probabilité p.
     """
+    
     i1 = np.random.randint(1, len(parcours))
     i2 = np.random.randint(1, len(parcours))
     while i2 == i1:
@@ -26,11 +31,12 @@ def mutation_echange(parcours):
     return nouveau_parcours
 
 
-def mutation_insertion(parcours):
+def mutation_insertion(parcours, p=0.1):
     """
     Choisit une ville au hasard (sauf la première), et l'insert à une place
-    au hasard sur le parcours (sauf au départ).
+    au hasard sur le parcours (sauf au départ) avec une probabilité p.
     """
+
     i_ville = np.random.randint(1, len(parcours))
     i_insertion = np.random.randint(1, len(parcours))
 
@@ -39,6 +45,46 @@ def mutation_insertion(parcours):
     nouveau_parcours = np.insert(nouveau_parcours, i_insertion, ville)
     return nouveau_parcours
 
+def hybridation_ox2(parcours1, parcours2):
+    
+    """Ref ; Genetic Algorithms for the Travelling Salesman Problem: A
+Review of Representations and Operators"""
+
+    n = len(parcours1)
+    a,b  = np.random.choice(range(1,n),2,replace=False)
+    a,b = min(a,b),max(a,b)
+        
+    parcours_start = parcours1[:a]
+    parcours_middle = parcours1[a:b]
+    parcours_end = parcours1[b:]
+    parcours_middle_new = []
+    
+    for ville in parcours2:
+        if ville in parcours_middle:
+            parcours_middle_new.append(ville)
+    
+    # print("here")     
+    # print(len(parcours_middle_new))
+    # print(len(parcours_middle))
+    # print()
+    # print(type(parcours_start))
+    # print(type(parcours_middle_new))
+    # print(type(parcours_end))
+    parcours_middle_new = np.array(parcours_middle_new)
+    parcours_new = np.concatenate((parcours_start, parcours_middle_new, parcours_end))
+    assert len(parcours_new) == n
+    
+    return parcours_new
+
+def enfanter (agent1, agent2):
+    parcours1 = agent1.parcours
+    parcours2 = agent2.parcours
+    
+    parcours = hybridation_ox2(parcours1, parcours2)
+    enfant = agent1.copier()
+    enfant.parcours = parcours
+    enfant.reevaluer()
+    return enfant
 
 def selectionner_tranche(agents, n_agents, ratio_meilleurs_scores, ratio_meilleurs_penalites):
     n_meilleurs_scores = int(n_agents * ratio_meilleurs_scores)
@@ -66,25 +112,23 @@ class Agent:
     score = 0
     distance = 0
     iteration = 0
-    p_mutation = 0.8
-    p_mutation_echange = 0.5
+    p_mutation_echange = 0.1
+    p_mutation_insertion = 0.1
 
-    def __init__(self, instance, dist_mat, parcours=None, p_mutation=0.8, p_mutation_echange=0.5):
+    def __init__(self, instance, dist_mat, parcours=None, p_mutation_echange=0.1, pm_mutation_insertion=0.1):
         self.instance = instance
         self.dist_mat = dist_mat
-        self.p_mutation = p_mutation
         self.p_mutation_echange = p_mutation_echange
+        self.p_mutation_insertion = pm_mutation_insertion
 
         if parcours:
             self.parcours = parcours
         else:
-            self.parcours = [1] + np.random.permutation(
-                np.arange(2, len(self.instance) + 1)
-            ).tolist()
+            self.parcours = np.random.permutation(np.arange(1, len(self.instance) + 1)).tolist()
         self.reevaluer()
 
     def act_erreur(self, erreur):
-        return 1000 + erreur * self.iteration
+        return erreur * self.iteration
 
     def iterer(self):
         self.iteration += 1
@@ -99,158 +143,132 @@ class Agent:
         return self
 
     def muter(self):
-        if np.random.rand() < self.p_mutation:
-            if np.random.rand() < self.p_mutation_echange:
-                self.parcours = mutation_echange(self.parcours)
-            else:
-                self.parcours = mutation_insertion(self.parcours)
+        if np.random.rand() < self.p_mutation_echange:
+            self.parcours = mutation_echange(self.parcours)
+        if np.random.rand() < self.p_mutation_insertion:
+            self.parcours = mutation_insertion(self.parcours)
         self.iterer()
         return self
 
-    def afficher_parcours(self):
-        import igraph as ig
-
-        coords = [(self.instance[i]["x"], self.instance[i]["y"]) for i in self.instance]
-        p = self.parcours
-        v = [(p[i] - 1, p[i + 1] - 1) for i in range(len(p) - 1)] + [(p[-1] - 1, p[0] - 1)]
-
-        g = ig.Graph()
-        g.add_vertices(self.parcours)
-        g.add_edges(v)
-
-        fig, ax = plt.subplots()
-
-        g.vs["label"] = g.vs["name"]
-        g.vs["label_size"] = 7
-        ig.plot(g, target=ax, layout=coords)
-
     def copier(self):
-        return copy.copy(self)
+        return copy.deepcopy(self)
 
 
 # %%
+# Run
 
-
-class Algorithme:
-    def __init__(
-        self,
-        instance,
-        dist_mat,
-        N_agents,
-        p_mutation=0.8,
-        p_mutation_echange=0.5,
-        ratio_parents=0.3,
-        ratio_meilleurs_scores_parents=0.2,
-        ratio_meilleurs_penalites_parents=0.1,
-        ratio_meilleurs_scores_enfants=0.2,
-        ratio_meilleurs_penalites_enfants=0.1,
-    ):
-        self.instance = instance
-        self.dist_mat = dist_mat
-
-        self.N_agents = N_agents
-
-        self.ratio_parents = ratio_parents
-        self.ratio_meilleurs_scores_parents = ratio_meilleurs_scores_parents
-        self.ratio_meilleurs_penalites_parents = ratio_meilleurs_penalites_parents
-        self.ratio_meilleurs_scores_enfants = ratio_meilleurs_scores_enfants
-        self.ratio_meilleurs_penalites_enfants = ratio_meilleurs_penalites_enfants
-
-        self.n_parents = int(self.N_agents * self.ratio_parents)
-        self.n_enfants = self.N_agents - self.n_parents
-
-        self.agents = [
-            Agent(instance, dist_mat, p_mutation=p_mutation, p_mutation_echange=p_mutation_echange)
-            for i in range(N_agents)
-        ]
-
-        self.iterations = [[agent.iteration for agent in self.agents]]
-        self.scores = [[agent.score for agent in self.agents]]
-        self.distances = [[agent.distance for agent in self.agents]]
-        self.n_penalites = [[agent.n_penalites for agent in self.agents]]
-
-    def iterer(self):
-        parents = [agent.copier().iterer() for agent in self.agents]
-        enfants = [agent.muter() for agent in self.agents]
-
-        parents_selectionnes = selectionner_tranche(
-            parents,
-            self.n_parents,
-            self.ratio_meilleurs_scores_parents,
-            self.ratio_meilleurs_penalites_parents,
-        )
-        enfants_selectionnes = selectionner_tranche(
-            enfants,
-            self.n_enfants,
-            self.ratio_meilleurs_scores_enfants,
-            self.ratio_meilleurs_penalites_enfants,
-        )
-
-        self.agents = parents_selectionnes + enfants_selectionnes
-        assert len(self.agents) == self.N_agents
-
-        self.iterations.append([agent.iteration for agent in self.agents])
-        self.scores.append([agent.score for agent in self.agents])
-        self.distances.append([agent.distance for agent in self.agents])
-        self.n_penalites.append([agent.n_penalites for agent in self.agents])
-
-    def lancer_simulation(self, N_iterations):
-        for i in tqdm(range(1, N_iterations + 1)):
-            algo.iterer()
-
-        self.afficher_simulation()
-
-    def afficher_simulation(self):
-        fig, (ax1, ax2, ax3) = plt.subplots(3, figsize=(8, 16), sharex=True)
-        ax3t = ax3.twinx()
-
-        ax1.scatter(
-            algo.iterations,
-            algo.n_penalites,
-            c="black",
-            marker=".",
-            alpha=0.4,
-        )
-        ax1.set_ylim([0, None])
-        ax1.set_ylabel("N pénalités")
-
-        ax2.scatter(
-            algo.iterations,
-            algo.scores,
-            c="black",
-            marker=".",
-            alpha=0.05,
-        )
-        ax2.set_ylabel("Scores")
-        ax2.set_yscale(("log"))
-
-        ax3.plot([min(score) for score in algo.scores], c="red")
-        ax3.set_ylabel("Meilleur score")
-        ax3.set_xlabel("Itération")
-
-        ax3t.plot([min(distance) for distance in algo.distances], c="black", ls="--")
-        ax3t.set_ylabel("Meilleure distance")
-
-        fig.tight_layout()
-        plt.show()
-
-
+total_time = 0
+start_time = time()
 instance = charger_instance("data/inst2")
 dist_mat = compute_dist_mat(instance)
 
-N_agents = 70
+N_agents = 100
 
+ratio_parents = 0.3
+ratio_meilleurs_scores_parents = 0.2
+ratio_meilleurs_penalites_parents = 0.1
+
+
+n_parents = int(N_agents * ratio_parents)
+n_enfants = N_agents - n_parents
+
+N_iteration = 1500
 continuer = False
-if not continuer:
-    algo = Algorithme(instance, dist_mat, N_agents)
 
-N_batches = 10
-N_iterations_par_batch = 400
-for batch in range(N_batches):
-    algo.lancer_simulation(N_iterations_par_batch)
+fig, (ax1, ax2, ax3) = plt.subplots(3, figsize=(8, 15), sharex=True)
+
+if not continuer:
+    
+    total_time = 0
+    agents = [Agent(instance, dist_mat, p_mutation_echange=0.35, pm_mutation_insertion=0.35) for i in range(N_agents)]
+
+    scores = [min(agent.score for agent in agents)]
+    distances = [min(agent.distance for agent in agents)]
+    n_penalites = [min(agent.n_penalites for agent in agents)]
+
+ax1.scatter(
+    [agent.iteration for agent in agents],
+    [agent.n_penalites for agent in agents],
+    c="black",
+    marker=".",
+    alpha=0.4,
+)
+ax2.scatter(
+    [agent.iteration for agent in agents],
+    [agent.score for agent in agents],
+    c="black",
+    marker=".",
+    alpha=0.05,
+)
+
+for i in range(1, N_iteration):
+    print(f"{i} / {N_iteration}", end="\r")
+
+    parents_selectionnes = selectionner_tranche(
+        agents,
+        n_parents,
+        ratio_meilleurs_scores_parents,
+        ratio_meilleurs_penalites_parents,
+    )
+    
+    enfants = []
+    for i in range(n_enfants):
+        parent1, parent2 = np.random.choice(parents_selectionnes, size=2, replace=False)
+        enfants.append(enfanter(parent1, parent2))
+
+    agents = parents_selectionnes + enfants
+    agents = [agent.muter() for agent in agents]
+    
+    
+    assert len(agents) == N_agents
+
+    scores.append(min(agent.score for agent in agents))
+    distances.append(min(agent.distance for agent in agents))
+    n_penalites.append(min(agent.n_penalites for agent in agents))
+
+    ax1.scatter(
+        [agent.iteration for agent in agents],
+        [agent.n_penalites for agent in agents],
+        c="black",
+        marker=".",
+        alpha=0.4,
+    )
+    ax2.scatter(
+        [agent.iteration for agent in agents],
+        [agent.score for agent in agents],
+        c="black",
+        marker=".",
+        alpha=0.05,
+    )
+
+
+run_time = time() - start_time
+total_time += run_time
+
+ax1.set_ylim([0, None])
+ax1.set_ylabel("N pénalités")
+ax2.set_ylabel("Scores")
+ax2.set_yscale(("log"))
+
+ax3.plot(scores, c="red")
+ax3.set_yscale("log")
+ax3.set_ylabel("Meilleur score")
+ax3.set_xlabel("Itération")
+ax3t = ax3.twinx()
+ax3t.plot(distances, c="black", ls="--")
+ax3t.set_ylabel("Meilleure distance")
+
+fig.tight_layout()
+
 
 # %%
-
+#End
 meilleur_agent = sorted(agents, key=lambda agent: agent.score)[0]
-print(meilleur_agent.parcours)
+print(f"Temps d'exécution total du run : {run_time:.2f} s")
+print("Meilleur score:", meilleur_agent.score)
+print("parcours:", meilleur_agent.parcours)
 meilleur_agent.afficher_parcours()
+
+
+
+# %%
