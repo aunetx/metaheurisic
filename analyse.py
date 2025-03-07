4882eed1b4626b5644f19f2228c62d30a7f0991e
Aurélien Hamy <me@aunetx.dev>
2025-03-07 02:17:08 +0100

    Plusieurs runs et meilleure analyse

--- analyse.py
+++ analyse.py
@@ -8,9 +8,9 @@ from utilitaires import *
 # %%
 
 
-def afficher_score_et_deviation(ax, data):
+def afficher_score_et_deviation(ax, data, N_agents):
     data[~np.isfinite(data)] = np.nan
-    x = np.arange(data.shape[1])
+    x = np.arange(data.shape[1]) * N_agents
     ax.fill_between(
         x,
         np.nanmin(data, axis=0),
@@ -20,6 +20,7 @@ def afficher_score_et_deviation(ax, data):
         color="firebrick",
         lw=0,
     )
+    data[~np.isfinite(data)] = np.inf
     ax.fill_between(
         x,
         np.nanquantile(data, 0.25, axis=0),
@@ -29,42 +30,47 @@ def afficher_score_et_deviation(ax, data):
         color="blue",
         lw=0,
     )
+    data[~np.isfinite(data)] = np.nan
     ax.plot(x, np.nanmean(data, axis=0), label="moyenne", c="black", ls=":")
+    data[~np.isfinite(data)] = np.inf
     ax.plot(x, np.nanmedian(data, axis=0), label="médiane", c="olivedrab", ls="--")
 
-    ax.set_xlim((0, len(x)))
+    ax.set_xlim((0, x[-1]))
     ax.legend()
 
 
-instance_name = "inst3"
-save_name = f"{instance_name}_sans_hybridation_70A"
+instance_name = "inst1"
+N_agents = 50
+save_name = f"{instance_name}_avec_hybridation_{N_agents}A"
 save_file_scores = f"best_scores/{save_name}_scores.txt"
 save_file_distances = f"best_scores/{save_name}_distances.txt"
 
 scores_array = np.array(lire_resultats(save_file_scores))
 distances_array = np.array(lire_resultats(save_file_distances))
+n_iterations = scores_array.shape[1] - 1
 
 plot_separes = False
 if plot_separes:
     for arr, name in zip([scores_array, distances_array], ["Score", "Distance"]):
         fig, ax = plt.subplots(figsize=(7, 5))
 
-        afficher_score_et_deviation(ax, arr)
+        afficher_score_et_deviation(ax, arr, N_agents)
+        ax.set_xlabel(f"Nombre d'évaluations ({N_agents} agents, {n_iterations} itérations)")
         ax.set_ylabel(name)
         ax.set_title(f"{name} et sa dispersion")
 
         fig.tight_layout()
         plt.show()
 else:
-    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 4))
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 3.5))
 
-    afficher_score_et_deviation(ax1, scores_array)
-    ax1.set_xlabel("Itération")
+    afficher_score_et_deviation(ax1, scores_array, N_agents)
+    ax1.set_xlabel(f"Nombre d'évaluations ({N_agents} agents, {n_iterations} itérations)")
     ax1.set_ylabel("Score")
     ax1.set_title("Score et sa dispersion")
 
-    afficher_score_et_deviation(ax2, distances_array)
-    ax2.set_xlabel("Itération")
+    afficher_score_et_deviation(ax2, distances_array, N_agents)
+    ax2.set_xlabel(f"Nombre d'évaluations ({N_agents} agents, {n_iterations} itérations)")
     ax2.set_ylabel("Distance")
     ax2.set_title("Distance et sa dispersion")
 
--- genetic.py
+++ genetic.py
@@ -10,20 +10,20 @@ from algorithm import *
 
 # %%
 
-instance_name = "inst3"
+instance_name = "inst1"
 instance = charger_instance(f"data/{instance_name}")
 dist_mat = compute_dist_mat(instance)
-save_name = f"{instance_name}_sans_hybridation_70A"
-save_file_scores = f"best_scores/{save_name}_scores.txt"
-save_file_distances = f"best_scores/{save_name}_distances.txt"
-
 SCORES_FILE_LOCK = multiprocessing.Lock()
 DISTANCES_FILE_LOCK = multiprocessing.Lock()
 
-N_agents = 70
+N_agents = 50
 N_batches = 10
-N_iterations_par_batch = 5000
-N_runs = 10
+N_iterations_par_batch = 50
+N_runs = 50
+
+save_name = f"{instance_name}_avec_hybridation_{N_agents}A"
+save_file_scores = f"best_scores/{save_name}_scores.txt"
+save_file_distances = f"best_scores/{save_name}_distances.txt"
 
 
 def run(run_number, verbose=True):
@@ -35,14 +35,14 @@ def run(run_number, verbose=True):
         N_agents,
         p_mutation_0=0.2,
         p_mutation_f=0.6,
-        iter_mutation_max=10000,
+        iter_mutation_max=2000,
         p_mutation_echange=0.5,
         ratio_parents=0.3,
         ratio_meilleurs_scores_parents=0.2,
         ratio_meilleurs_penalites_parents=0.2,
         ratio_meilleurs_scores_enfants=0.2,
         ratio_meilleurs_penalites_enfants=0.2,
-        utiliser_hybridation=False,
+        utiliser_hybridation=True,
     )
 
     for batch in range(N_batches):
@@ -79,7 +79,7 @@ else:
 
 # %%
 
-meilleur_agent = sorted(algo_list[0].agents, key=lambda agent: agent.score)[0]
+meilleur_agent = sorted(algo_list[3].agents, key=lambda agent: agent.score)[0]
 print(f"meilleur score : {int(meilleur_agent.score)}")
 print(meilleur_agent.parcours)
 meilleur_agent.afficher_parcours()
